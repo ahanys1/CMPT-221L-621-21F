cd747b5da100a30f1560624542f47297ecaafb9b
Alex Hanys <abhmind@gmail.com>
2021-10-06 18:46:28 -0400

    Update script.js

--- Labs/Lab 5/Submission/script.js
+++ Labs/Lab 5/Submission/script.js
@@ -1,7 +1,7 @@
 document.getElementById("top").innerHTML="new text";
 document.getElementById("bossy").innerHTML = "Please do these items below.";
 document.getElementById("blue").style.color = "blue";
-document.getElementById("cream").style.backgroundColor = "cream";
+document.getElementById("cream").style.backgroundColor = "#FFFDD0";
 document.body.style.backgroundColor = "gray";
 document.getElementById("upper").style.textTransform = "uppercase";
 document.getElementById("last").classList.add("title");
