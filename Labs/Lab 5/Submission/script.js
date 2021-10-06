129ae2db0448abc42e7d67d1fbcff48ec92133e7
Alex Hanys <abhmind@gmail.com>
2021-10-06 15:51:35 -0400

    lab 5

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,7 @@
+document.getElementById("top").innerHTML="new text";
+document.getElementById("bossy").innerHTML = "Please do these items below.";
+document.getElementById("blue").style.color = "blue";
+document.getElementById("cream").style.backgroundColor = "cream";
+document.body.style.backgroundColor = "gray";
+document.getElementById("upper").style.textTransform = "uppercase";
+document.getElementById("last").classList.add("title");
